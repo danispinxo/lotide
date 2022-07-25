874bca4461c4ab561457b0594190e5fb7109011c
danispinxo <genericpronoun@gmail.com>
2022-07-25 18:24:26 +0000

    Tail function to take parts of arrasy that are not the head

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = (arr) => {
+  let tailArray = [];
+  for (let i = 1; i < arr.length; i++) {
+    tailArray.push(arr[i]);
+  }
+  return tailArray;
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words);
+assertEqual(words.length, 3);
