44f07bf97e51db7771712bc741084c8e43ebbd25
danispinxo <genericpronoun@gmail.com>
2022-07-26 16:51:24 +0000

    adding file assertArraysEqual -- checks if arrays are identical

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(actual, expected) {
+  let match = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      match = false;
+    }
+  }
+  return match;
+};
+
+const assertArraysEqual = function(array1, array2) {
+
+  if (eqArrays(array1, array2)) {
+    console.log(`👍 Assertion Passed! ${array1} and ${array2} are equal!`);
+  } else {
+    console.log(`👎 Assertion Failed. ${array1} and ${array2} are not equal.`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 4], [1, 2, 3]);
+assertArraysEqual([1, 2, '3'], [1, 2, 3]);
+assertArraysEqual(["Hi", "I'm", "Dani"], ["Hi", "I'm", "Dani"]);
+assertArraysEqual(["Hi", "I'm", "Dani"], ["Hi", "I'm", "Jesse"]);
