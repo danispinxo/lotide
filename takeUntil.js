6717d847662e1956552be825318c1015e6d62456
danispinxo <genericpronoun@gmail.com>
2022-07-28 13:51:14 +0000

    adding file takeUntil.js -- returns an array until an item in it meets a callback function condition

---
+++ takeUntil.js
@@ -0,0 +1,40 @@
+const eqArrays = function(actual, expected) {
+  let match = true;
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      match = false;
+    }
+  }
+  return match;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`👍 Assertion Passed! ${array1} and ${array2} are equal!`);
+  } else {
+    console.log(`👎 Assertion Failed. ${array1} and ${array2} are not equal.`);
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
