98f03916d23d77d3cfe4e84ccbdb46b994a10fec
danispinxo <genericpronoun@gmail.com>
2022-07-29 18:30:35 +0000

    eqObjects now deals with nested objects properly

--- eqObjects.js
+++ eqObjects.js
@@ -20,33 +20,35 @@ const eqArrays = function(actual, expected) {
 };
 
 const eqObjects = function(object1, object2) {
-  let match = true;
-
   for (let key1 in object1) {
     for (let key2 in object2) {
-      if (
-        !object2[key1] ||
-        !object1[key2] ||
-        object2[key1] !== object1[key1] ||
-        object1[key2] !== object2[key2]
-      ) {
-        match = false;
+      if (!object2[key1] || !object1[key2]) {
+        return false;
       }
       if (Array.isArray(object1[key1]) || Array.isArray(object2[key2])) {
-        if (eqArrays(object1[key1], object2[key1])) {
-          match = true;
-        } else {
-          match = false;
+        if (!eqArrays(object1[key1], object2[key1])) {
+          return false;
+        } 
+      } else if (typeof object1[key1] === "object" || typeof object2[key2] === "object") {
+        if (!eqObjects(object1[key1], object2[key2])) {
+          return false;
+        } 
+      } else {
+        if (object2[key1] !== object1[key1] || object1[key2] !== object2[key2]) {
+          return false;
         }
       }
     }
   }
-  return match;
+  return true;
 };
+
+
 
 const ab = { a: "1", b: "2" };
 const ba = { b: "2", a: "1" };
 assertEqual(eqObjects(ab, ba), true);
+
 
 const abc = { a: "1", b: "2", c: "3" };
 assertEqual(eqObjects(ab, abc), false);
@@ -57,3 +59,7 @@ assertEqual(eqObjects(cd, dc), true);
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false);
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); 
