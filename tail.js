a9d4b41ffc3a3ab4fea137a79ae586e86a8a994f
danispinxo <genericpronoun@gmail.com>
2022-08-01 11:41:06 +0000

    Refactoring tail.js using test files & module exports

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require("././assertEqual");
 
 const tail = (arr) => {
   let tailArray = [];
@@ -14,11 +8,4 @@ const tail = (arr) => {
   return tailArray;
 };
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
