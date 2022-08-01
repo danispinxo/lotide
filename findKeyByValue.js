8dad07d5dfe9389ea5d80413e98457ec23b14b8f
danispinxo <genericpronoun@gmail.com>
2022-08-01 15:33:33 +0000

    Refactoring findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`👍 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`👎 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(obj, value) {
   for (let key in obj) {
     if (obj[key] === value) {
@@ -14,11 +6,4 @@ const findKeyByValue = function(obj, value) {
   }
 };
 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
